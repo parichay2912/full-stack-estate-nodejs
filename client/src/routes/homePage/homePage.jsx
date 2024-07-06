fc48f2477e35b592d3e7f5572e9b1d59f85cd916
Your Name <parichay@rubick.ai>
2024-07-06 08:29:37 +0530

    added nodejs application

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import SearchBar from '../../components/searchBar/SearchBar';
+import './homePage.scss';
+import { AuthContext } from '../../context/AuthContext';
+
+function HomePage() {
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Discovering real estate near you is effortless with our website's
+            intuitive features. By simply allowing access to your location, our
+            platform swiftly identifies properties that match your preferences
+            and are conveniently situated nearby.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
